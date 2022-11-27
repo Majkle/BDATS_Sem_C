ac4b3bf5e6e93241b8fb1ff1f910c4a0d35a5e86
Michael Pinkas <michael.pinkas@unicorn.com>
2022-11-27 08:59:26 +0100

    AH - tests

--- src/main/test/pinkas/michael/sem/c/struktury/heap/AbstrHeapTest.java
+++ src/main/test/pinkas/michael/sem/c/struktury/heap/AbstrHeapTest.java
@@ -5,6 +5,11 @@ import org.junit.Before;
 import org.junit.Test;
 import pinkas.michael.sem.c.TestData;
 
+import java.util.Arrays;
+import java.util.Iterator;
+
+import static org.junit.Assert.assertTrue;
+
 public class AbstrHeapTest {
     private TestData[] data;
     private AbstrHeap<TestData> abstrHeap;
@@ -28,9 +33,48 @@ public class AbstrHeapTest {
         abstrHeap = null;
     }
 
+    private boolean jeOcekavanyObsahFronty(TestData[] ocekavany, AbstrHeap<TestData> vysledek) {
+        Iterator<TestData> iterator = vysledek.iterator(null);
+        for (int i = 0; i < ocekavany.length; i++) {
+
+            System.out.println(ocekavany[i]);
+            if (ocekavany[i].compareTo(iterator.next()) != 0) {
+                System.out.println("xd");
+                return false;
+            }
+        }
+
+        return !iterator.hasNext();
+    }
 
     @Test
-    public void abstrHeapTest_zpristupniMax() {
+    public void abstrHeapTest_vybuduj_1() {
+        data = new TestData[3];
+
+        data[0] = T0;
+        data[1] = T1;
+        data[2] = T2;
+
+        abstrHeap.vybuduj(data);
+
+        assertTrue(jeOcekavanyObsahFronty(new TestData[]{T0, T1, T2}, abstrHeap));
+    }
+
+    @Test
+    public void abstrHeapTest_vybuduj_2() {
+        data = new TestData[3];
+
+        data[0] = T2;
+        data[1] = T1;
+        data[2] = T0;
+
+        abstrHeap.vybuduj(data);
+
+        assertTrue(jeOcekavanyObsahFronty(new TestData[]{T1, T2, T3}, abstrHeap));
+    }
+
+    @Test
+    public void abstrHeapTest_vybuduj_3() {
         data = new TestData[3];
 
         data[0] = T0;
@@ -39,6 +83,151 @@ public class AbstrHeapTest {
 
         abstrHeap.vybuduj(data);
 
+        assertTrue(jeOcekavanyObsahFronty(new TestData[]{T1, T2, T3}, abstrHeap));
+    }
+
+    @Test
+    public void abstrHeapTest_vybuduj_4() {
+        data = new TestData[6];
+
+        data[0] = T0;
+        data[1] = T1;
+        data[2] = T2;
+        data[3] = T3;
+        data[4] = T4;
+        data[5] = T5;
+
+        abstrHeap.vybuduj(data);
+
+        assertTrue(jeOcekavanyObsahFronty(new TestData[]{T0, T1, T2, T3, T4, T5}, abstrHeap));
+    }
+
+    @Test
+    public void abstrHeapTest_vybuduj_5() {
+        data = new TestData[6];
+
+        data[0] = T5;
+        data[1] = T4;
+        data[2] = T3;
+        data[3] = T2;
+        data[4] = T1;
+        data[5] = T0;
+
+        abstrHeap.vybuduj(data);
+
+        assertTrue(jeOcekavanyObsahFronty(new TestData[]{T0, T1, T2, T3, T4, T5}, abstrHeap));
+    }
+
+    @Test
+    public void abstrHeapTest_vybuduj_6() {
+        data = new TestData[6];
+
+        data[0] = T1;
+        data[1] = T3;
+        data[2] = T4;
+        data[3] = T2;
+        data[4] = T5;
+        data[5] = T0;
+
+        abstrHeap.vybuduj(data);
+
+        assertTrue(jeOcekavanyObsahFronty(new TestData[]{T0, T1, T2, T3, T4, T5}, abstrHeap));
+    }
+
+    @Test
+    public void abstrHeapTest_prebuduj_1() {
+        data = new TestData[3];
+
+        data[0] = T0;
+        data[1] = T1;
+        data[2] = T2;
+
+        abstrHeap.vybuduj(data);
+
+        abstrHeap.vloz(T3);
+
+        assertTrue(jeOcekavanyObsahFronty(new TestData[]{T0, T1, T2,T3}, abstrHeap));
+    }
+
+    @Test
+    public void abstrHeapTest_prebuduj_2() {
+        data = new TestData[3];
+
+        data[0] = T2;
+        data[1] = T1;
+        data[2] = T3;
+
+        abstrHeap.vybuduj(data);
+
+        abstrHeap.vloz(T0);
+
+        assertTrue(jeOcekavanyObsahFronty(new TestData[]{T0, T1, T2,T3}, abstrHeap));
+    }
+
+    @Test
+    public void abstrHeapTest_prebuduj_3() {
+        data = new TestData[3];
+
+        data[0] = T0;
+        data[1] = T3;
+        data[2] = T1;
+
+        abstrHeap.vybuduj(data);
+
+        abstrHeap.vloz(T2);
+
+        assertTrue(jeOcekavanyObsahFronty(new TestData[]{T0, T1, T2,T3}, abstrHeap));
+    }
+
+    @Test
+    public void abstrHeapTest_prebuduj_4() {
+        data = new TestData[5];
+
+        data[0] = T0;
+        data[1] = T1;
+        data[2] = T2;
+        data[3] = T3;
+        data[4] = T4;
+
+        abstrHeap.vybuduj(data);
+
+        abstrHeap.vloz(T5);
+
+        assertTrue(jeOcekavanyObsahFronty(new TestData[]{T0, T1, T2, T3, T4, T5}, abstrHeap));
+    }
+
+    @Test
+    public void abstrHeapTest_prebuduj_5() {
+        data = new TestData[5];
+
+        data[0] = T5;
+        data[1] = T4;
+        data[2] = T2;
+        data[3] = T1;
+        data[4] = T0;
+
+        abstrHeap.vybuduj(data);
+
+        abstrHeap.vloz(T3);
+
+        assertTrue(jeOcekavanyObsahFronty(new TestData[]{T0, T1, T2, T3, T4, T5}, abstrHeap));
+    }
+
+    @Test
+    public void abstrHeapTest_prebuduj_6() {
+        data = new TestData[5];
+
+        data[0] = T1;
+        data[1] = T3;
+        data[2] = T4;
+        data[3] = T2;
+        data[4] = T5;
+
+        abstrHeap.vybuduj(data);
+
+        abstrHeap.vloz(T0);
+
+        assertTrue(jeOcekavanyObsahFronty(new TestData[]{T0, T1, T2, T3, T4, T5}, abstrHeap));
     }
 
     //TODO iterator
